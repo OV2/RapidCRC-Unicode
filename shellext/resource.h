6181004ec9eebc87adf9fb78eac854bf1c5e39b7
OV2 <OV2@sessionclan.de>
2011-08-03 00:39:27 +0200

    Add shell extension project

---
+++ shellext/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by rcrcshex1.rc
+//
+#define IDI_CREATE_SFV                  103
+#define IDI_OPEN_WITH                   104
+#define IDI_RAPIDCRC                    105
+#define IDI_OPEN_FOLDER                 106
+#define IDI_HASH_FILE                   107
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
