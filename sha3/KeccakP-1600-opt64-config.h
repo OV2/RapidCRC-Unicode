4f946f5d26a450bbcc93e1cca5321dc6b34487a0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-29 18:06:13 +0800

    Delete sha3 directory

--- sha3/KeccakP-1600-opt64-config.h
+++
@@ -1,8 +0,0 @@
-/*
-This file defines some parameters of the implementation in the parent directory.
-*/
-
-#define KeccakP1600_implementation_config "lane complementing, all rounds unrolled, using SHLD for rotations"
-#define KeccakP1600_fullUnrolling
-#define KeccakP1600_useLaneComplementing
-#define KeccakP1600_useSHLD
